a7465a06d21a814f9fde40c0e36de3d0290e5526
Antony rossi de jesus <antonyrossidejesus@gmail.com>
2024-06-26 22:31:01 -0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+const withAbsolutePath = require("next-absolute-path");
+
+module.exports = withAbsolutePath()({});
+
 export default nextConfig;
